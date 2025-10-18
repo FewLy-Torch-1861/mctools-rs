d60e257098685f3287431d68c6d578645c191244
ナッティ <furytorch.1861@gmail.com>
2025-10-18 11:55:40 +0700

    add nether coord converter!!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(help_template = "Simple misc minecraft tools.\n\n{usage-heading}\n{all-args}")]
+struct Cli {
+  #[command(subcommand)]
+  command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+  /// Convert Overworld coordinates to Nether coordinates.
+  #[command(allow_hyphen_values = true)]
+  NetherCoord {
+    #[arg(short, long, help = "Convert nether to overworld instead.")]
+    reverse: bool,
+
+    #[arg(help = "X position")]
+    x: i32,
+
+    #[arg(help = "Z position")]
+    z: i32,
+  },
+}
+
 fn main() {
-    println!("Hello, world!");
+  let cli: Cli = Cli::parse();
+
+  match cli.command {
+    Commands::NetherCoord { reverse, x, z } => {
+      if reverse {
+        convert_to_overworld(x, z);
+      } else {
+        convert_to_nether(x, z);
+      }
+    }
+  }
+}
+
+fn convert_to_nether(x: i32, z: i32) {
+  let x_nether: f64 = (x as f64) / 8.0;
+  let z_nether: f64 = (z as f64) / 8.0;
+
+  println!("xz: {}, {}", x_nether, z_nether);
+}
+
+fn convert_to_overworld(x: i32, z: i32) {
+  let x_ow: f64 = (x as f64) * 8.0;
+  let z_ow: f64 = (z as f64) * 8.0;
+
+  println!("xz: {}, {}", x_ow, z_ow);
 }
